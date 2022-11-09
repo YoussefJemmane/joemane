12f2daed858bed649eac35d761e0a5346c5abb2d
YoussefJemmane <JEMMANE.YOUSSEF@ofppt-edu.ma>
2022-11-09 16:25:23 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -39,27 +39,7 @@ export default function Home() {
           <a  className={styles.card}>
             <h2>Hobbies &rarr;</h2>
             <p>I love solving ciphers and playing BasketBall, I adore playing AAA and RPG Games My favourite game is The legends of Zelda</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          </a>   
         </div>
       </main>
 
