f846cd34971ededede950cb98e869fe847908584
YoussefJemmane <JEMMANE.YOUSSEF@ofppt-edu.ma>
2022-11-10 20:11:39 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
-import '../styles/globals.css'
 import { Analytics } from '@vercel/analytics/react';
-function MyApp({ Component, pageProps }) {<>
-  return <Component {...pageProps} />
-  <Analytics />
-  </>
+import '../styles/globals.css';
+function MyApp({ Component, pageProps }) {
+
+  return (
+    <><Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
 
 export default MyApp
