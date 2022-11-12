2feb86614180b8f9480261254f6157c7958ebbd4
YoussefJemmane <JEMMANE.YOUSSEF@ofppt-edu.ma>
2022-11-12 20:12:50 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,11 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.html",
+    "./node_modules/flowbite-react/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
+  plugins: [require('daisyui'),require('flowbite/plugin')],
 }
