49c5533bd6ede4d9a496e6ddce9c0662754600b7
YoussefJemmane <JEMMANE.YOUSSEF@ofppt-edu.ma>
2022-11-13 15:10:13 +0100

    add french

--- pages/lang/index.jsx
+++ pages/lang/index.jsx
@@ -3,14 +3,13 @@
 /* eslint-disable jsx-a11y/alt-text */
 
 import Head from 'next/head';
-import Link from 'next/link';
 import { Client, HydrationProvider } from "react-hydration-provider";
 
 
 
 
 export default function Home() {
-
+  
   const data = {
     name: 'Youssef Jemmane',
   }
@@ -26,23 +25,15 @@ export default function Home() {
           </Head>
 
           <main className='m-5'>
-            <div className='flex justify-end' >
-              <div className="dropdown dropdown-left dropdown-hover" >
-                <label tabIndex={0} className="btn m-1 btn-primary" data-theme='light'>Hover</label>
-                <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52" data-theme='light'>
-                  <li><Link href='/'>English</Link></li>
-                  <li><Link href='/lang'>French</Link></li>
-                </ul>
-              </div>
-            </div>
+            
             <div className='flex justify-center' >
               <div className="card w-96 bg-base-100 shadow-xl" data-theme='light'>
                 <figure ><img src="/p.jfif" width={1000} height={100} effects="blur" /></figure>
                 <div className="card-body ">
-                  <h2 className="card-title">My name is {data.name}</h2>
-                  <p>I'm studying Full Stack development at OFPPT in Morocco</p>
+                  <h2 className="card-title">Je m'appelle {data.name}</h2>
+                  <p>J'étudie le développement Full Stack à l'OFPPT au Maroc</p>
                   <div className="card-actions justify-center">
-                    <a href="./CV.pdf" className='btn btn-primary' download>This is My CV</a>
+                    <a href="./CV.pdf" className='btn btn-primary' download>Mon CV</a>
                   </div>
                 </div>
               </div>
@@ -52,14 +43,14 @@ export default function Home() {
                 <div className="flex items-center p-4  rounded-lg shadow-sm  " data-theme='light'>
                   <div className="card-body">
                     <h2 className="card-title">Skills &rarr;</h2>
-                    <p>I'm able to create a CRUD website using PHP and MySQL or Laravel, and I have the knowledge to create a UI using React and Redux</p>
+                    <p>Je suis capable de créer un site Web CRUD en utilisant PHP et MySQL ou Laravel, et j'ai les connaissances nécessaires pour créer une interface utilisateur en utilisant React et Redux</p>
 
                   </div>
                 </div>
                 <div className="flex items-center p-4  rounded-lg shadow-sm  " data-theme='light'>
                   <div className="card-body">
                     <h2 className="card-title">Hobbies &rarr;</h2>
-                    <p>I love solving ciphers and playing BasketBall, I adore playing AAA and RPG Games My favourite game is The legends of Zelda</p>
+                    <p>J'adore résoudre des chiffres et jouer au BasketBall, j'adore jouer aux jeux AAA et RPG Mon jeu préféré est The legends of Zelda</p>
                   </div>
                 </div>
                 <div className="flex items-center p-4  rounded-lg shadow-sm  " data-theme='light'>
@@ -70,7 +61,7 @@ export default function Home() {
                   </div>
                 </div>
               </div>
-
+              
             </section>
 
 
@@ -90,12 +81,12 @@ export default function Home() {
                   <path d="M15,3C8.373,3,3,8.373,3,15c0,6.627,5.373,12,12,12s12-5.373,12-12C27,8.373,21.627,3,15,3z M10.496,8.403 c0.842,0,1.403,0.561,1.403,1.309c0,0.748-0.561,1.309-1.496,1.309C9.561,11.022,9,10.46,9,9.712C9,8.964,9.561,8.403,10.496,8.403z M12,20H9v-8h3V20z M22,20h-2.824v-4.372c0-1.209-0.753-1.488-1.035-1.488s-1.224,0.186-1.224,1.488c0,0.186,0,4.372,0,4.372H14v-8 h2.918v1.116C17.294,12.465,18.047,12,19.459,12C20.871,12,22,13.116,22,15.628V20z"></path>
                 </svg></a>
                 <a href='https://github.com/YoussefJemmane'><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                  width="24" height="24"
-                  viewBox="0 0 28 24"
-                  className="fill-current">
-                  <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
-                </svg>
-                </a>
+width="24" height="24"
+viewBox="0 0 28 24"
+className="fill-current">
+    <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
+</svg>
+</a>
 
               </div>
             </div>
