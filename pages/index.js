610f1e38c6628dd443124296d1b38e025346c5ad
YoussefJemmane <JEMMANE.YOUSSEF@ofppt-edu.ma>
2023-04-27 13:48:25 +0100

    update CV

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,27 @@
 /* eslint-disable jsx-a11y/alt-text */
 
 import Head from 'next/head';
-import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import { Client, HydrationProvider } from "react-hydration-provider";
 
 
 
 
 export default function Home() {
+  const [repo, setRepo] = useState(null)
+  const [isLoading, setLoading] = useState(false)
 
+  useEffect(() => {
+    setLoading(true)
+    fetch('https://api.github.com/users/YoussefJemmane/repos')
+      .then((res) => res.json())
+      .then((repo) => {
+        setRepo(repo)
+        setLoading(false)
+      })
+  }, [])
+  if (isLoading) return <p>Loading...</p>
+  if (!repo) return <p>No profile data</p>
   const data = {
     name: 'Youssef Jemmane',
   }
@@ -30,56 +43,22 @@ export default function Home() {
               <div class="flex-1">
                 <a class="btn btn-ghost normal-case text-xl">Youssef Jemmane</a>
               </div>
-              <div class="flex-none">
-                <div className="dropdown dropdown-left dropdown-hover" >
-                  <label tabIndex={0} className="btn m-1 btn-primary" data-theme='light'>Translate</label>
-                  <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52" data-theme='light'>
-                    <li><Link href='/'>English</Link></li>
-                    <li><Link href='/lang'>French</Link></li>
-                  </ul>
-                </div>
-              </div>
+
             </div>
             <div className='flex justify-center m-5' >
               <div className="card w-96 bg-base-100 shadow-xl" data-theme='light'>
                 <figure className='m-5'><img className='rounded-xl' src="/p.jfif" width={1000} height={100} effects="blur" /></figure>
                 <div className="card-body ">
-                  <h2 className="card-title">My name is {data.name}</h2>
-                  <p>I'm studying Full Stack development at OFPPT in Morocco</p>
-                  <div className="card-actions justify-center">
-                    <a href="./YJ.pdf" className='btn btn-primary' download>This is My CV</a>
+                  <h2 className="card-title">My name is {data.name} <br/> I'm Full Stack Developer</h2>
+                  
+                  <div className="card-actions justify-center pt-5">
+                    <a href="./Youssef_Jemmane.pdf" className='btn btn-primary' download>This is My CV</a>
                   </div>
                 </div>
               </div>
             </div>
             <section className='container px-6 py-4 mx-auto'>
-              <div className='grid gap-6 mb-8 md:grid-cols-1 lg:grid-cols-1'>
-                <div className="flex items-center p-4  rounded-lg shadow-sm  " data-theme='light'>
-                  <div className="card-body">
-                    <h2 className="card-title">Profile &rarr;</h2>
-                    <p>
-                      As a full stack student at OFPPT, I have developed a strong knowledge of front-end and back-end technologies, including <strong>React</strong>, <strong>Redux</strong>, <strong>the Redux Toolkit</strong>, <strong>API development</strong>, <strong>Laravel</strong>, <strong>PHP</strong>, and <strong>MySQL</strong>. I am skilled in building modern, dynamic web applications that provide users with a seamless and intuitive experience.
-                      I am passionate about using my skills and knowledge to create innovative solutions that solve real-world problems, and I am always looking for new challenges and opportunities to learn and grow as a developer. In the future, I hope to continue learning and advancing my skills, and to eventually use my expertise to make a positive impact on the world through technology.
-                    </p>
-
-                  </div>
-                </div>
-              </div>
-              <div className='grid gap-6 mb-8 md:grid-cols-1 lg:grid-cols-2'>
-                <div className="flex items-center p-4  rounded-lg shadow-sm  " data-theme='light'>
-                  <div className="card-body">
-                    <h2 className="card-title">Skills &rarr;</h2>
-                    <p>I am skilled in creating CRUD websites using PHP, MySQL, Laravel, React, and Redux. I am able to combine my knowledge of front-end and back-end technologies to create well-rounded and user-friendly web applications. I am passionate about using technology to solve real-world problems, and I am always looking for new opportunities to learn and grow as a developer.</p>
-
-                  </div>
-                </div>
-                <div className="flex  items-center p-4 rounded-lg shadow-sm  " data-theme='light'>
-                  <div className="card-body">
-                    <h2 className="card-title">Hobbies &rarr;</h2>
-                    <p>I am passionate about solving ciphers, playing basketball, and playing video games. These interests have helped me to develop a wide range of valuable skills, including problem-solving, coordination, teamwork, strategic thinking, and critical analysis. My favorite game, The Legend of Zelda, has particularly inspired me to develop my creativity and problem-solving abilities.</p>
-                  </div>
-                </div>
-              </div>
+              
 
               <div className="flex  items-center p-4 rounded-lg shadow-sm  " data-theme='light'>
                 <div className="card-body">
@@ -139,41 +118,29 @@ export default function Home() {
                   <h2 className="card-title">Projects &rarr;</h2>
                   <div>
                     <div className='grid gap-6 mb-8 max-sm:grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3'>
-                      <div className="flex items-center rounded-lg shadow-sm  " data-theme='synthwave'>
-                        <div className="card-body">
-                          <h2 className="card-title">Cinema Management &rarr; Laravel</h2>
-                          <div>
-                            <a href="https://github.com/YoussefJemmane/Cinema_Management.git" className='btn btn-secondary'>GitHub</a>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="flex items-center rounded-lg shadow-sm  " data-theme='synthwave'>
-                        <div className="card-body">
-                          <h2 className="card-title">Dice &rarr; React, Redux</h2>
-                          <div>
-                            <a href="https://github.com/YoussefJemmane/Dice.git" className='btn btn-secondary mr-5 mb-5'>GitHub</a>
-                            <a href="https://dice-joe.vercel.app" className='btn btn-success'>Demo</a>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="flex items-center rounded-lg shadow-sm  " data-theme='synthwave'>
-                        <div className="card-body">
-                          <h2 className="card-title">Newspaper &rarr; React, Redux Toolkit</h2>
-                          <div>
-                            <a href="https://github.com/YoussefJemmane/articles.git" className='btn btn-secondary mr-5 mb-5'>GitHub</a>
-                            <a href="https://article-jemmane.vercel.app" className='btn btn-success'>Demo</a>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="flex items-center rounded-lg shadow-sm  " data-theme='synthwave'>
-                        <div className="card-body">
-                          <h2 className="card-title">Show Countries &rarr; React, API</h2>
-                          <div>
-                            <a href="https://github.com/YoussefJemmane/RESTCountries.git" className='btn btn-secondary mr-5 mb-5'>GitHub</a>
-                            <a href="https://joe-country.vercel.app" className='btn btn-success'>Demo</a>
-                          </div>
-                        </div>
-                      </div>
+                      {repo.map((repo) => {
+                        return (
+                          <>
+                            {
+                              repo.size === 0 ? null :
+                                <div className="flex items-center rounded-lg shadow-sm  " data-theme='synthwave'>
+                                  <div className="card-body">
+                                    <h2 className="card-title">{repo.name} &rarr; {repo.language}</h2>
+                                    <div>
+                                      <a href={repo.html_url} className='btn btn-secondary mr-5 mb-5'>GitHub</a>
+                                      {/* if homepage ==null don't show demo */}
+                                      {repo.homepage ? <a href={repo.homepage} className='btn btn-primary'>Demo</a> : null}
+                                    </div>
+                                  </div>
+                                </div>
+                            }
+
+                          </>
+
+
+                        )
+                      })}
+
                     </div>
                   </div>
                 </div>
